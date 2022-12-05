6cf4ba004fdb0e14554e72fa112a00222419fb49
Milan Hauth <milahu@gmail.com>
2022-12-05 15:12:30 +0100

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import UnpluginIcons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
@@ -6,6 +9,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 import { prefetch } from './prefetch-plugin';
 import path from 'path'
+import fs from 'fs'
 
 const assetsDir = '';
 const outputDefaults = {
@@ -23,6 +27,8 @@ export default defineConfig({
     prefetch(),
 
     UnpluginIcons({ autoInstall: true, compiler: 'svelte' }),
+
+    // service worker is used in src/svelte/components/Desktop/SystemUpdate.svelte
     VitePWA({
       includeAssets: [
         'robots.txt',
@@ -104,16 +110,87 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '$src': path.resolve('./src/svelte')
+      '$src': path.resolve('./src/svelte'),
+
+      /*
+      // https://github.com/jvilk/BrowserFS
+      'fs': 'browserfs/dist/shims/fs.js',
+      'buffer': 'browserfs/dist/shims/buffer.js',
+      'path': 'browserfs/dist/shims/path.js',
+      'processGlobal': 'browserfs/dist/shims/process.js',
+      'bufferGlobal': 'browserfs/dist/shims/bufferGlobal.js',
+      'bfsGlobal': require.resolve('browserfs'),
+      */
+
     },
   },
   build: {
     outDir: path.resolve('./docs'),
+    emptyOutDir: true, // fix: outDir is not inside project root
     target: 'esnext',
     minify: false,
     rollupOptions: {
       output: {
         ...outputDefaults,
+        manualChunks(id) {
+          // bundle dependencies manually
+          // TODO do this automatically
+          // see also https://github.com/sanyuan0704/vite-plugin-chunk-split/issues/18
+          /*
+          if (
+            id.includes('/node_modules/@codemirror/') ||
+            id.includes('/node_modules/style-mod/') ||
+            id.includes('/node_modules/w3c-keyname/') ||
+            false
+          ) {
+            return 'codemirror';
+          }
+          if (
+            id.includes('/node_modules/@lezer/') ||
+            false
+          ) {
+            return 'lezer';
+          }
+          console.log("id", id)
+          if (id.includes('/node_modules/')) {
+            return 'vendor';
+          }
+          */
+          const nodeModulesFolder = "node_modules/";
+          //const nodeModulesFolder = ".npm/";
+          //const nodeModulesFolder = "lib/";
+          console.log("id", id)
+          // simple: bundle each module in a separate file
+          if (id.includes('/node_modules/.pnpm/')) {
+            // example: /tmp/asdf/node_modules/.pnpm/@codemirror+view@6.6.0/node_modules/@codemirror/view/dist/index.js
+            var m = id.match(/\/node_modules\/\.pnpm\/([^/]+)\//);
+            if (m && m[1]) {
+              // example: @codemirror+view@6.6.0
+              // vite will convert "+" to "_"
+              return nodeModulesFolder + m[1];
+            }
+            return nodeModulesFolder + "noname";
+          }
+          if (id.includes('/node_modules/')) {
+            // example: /tmp/asdf/node_modules/@codemirror/view/dist/index.js
+            var m = id.match(/^.*?\/node_modules\/(@[^/]+\/[^/]+|[^/]+)\//);
+            if (m && m[1]) {
+              // example: @codemirror/view
+              // vite will convert "+" to "_"
+              var name = m[1].replace("/", "+")
+              // get version
+              var pkgFile = m + "package.json"
+              if (fs.existsSync(pkgFile)) {
+                var pkg = fs.readFileSync(pkgFile, "utf8");
+                if (pkg.version) {
+                  name += "@" + pkg.version;
+                }
+              }
+              return nodeModulesFolder + name;
+            }
+            return nodeModulesFolder + "noname";
+          }
+        },
       }
     },
   },
